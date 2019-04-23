4c10b618088c9a39f17c77c0a6a7ed6ef7958d08
SupremeLin <759698315@qq.com>
2019-04-23 14:35:28 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from djando.shortcuts import redirects
 
 
 def index(requeset):
 	return HttpResponse('/index')
+
+
+def login(request):
+	return redirect('/index')
